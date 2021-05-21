4bfdf80fc45a1e63bda2e5f54bbbf133ee8d585e
Leonardo <Leonardo@LAPTOP-L0DNUSFL>
2021-05-21 08:34:10 -0300

    Inclusão da Class Pessoa

--- HandleExcelWithApachePoi/src/ApachePoi.java
+++ HandleExcelWithApachePoi/src/ApachePoi.java
@@ -1,8 +1,17 @@
+import java.io.File;
+import java.io.IOException;
 
 public class ApachePoi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		File file = new File("C:\\Users\\Leonardo\\git\\repository5\\HandleExcelWithApachePoi\\src\\arquivo_excel.xls");
+		
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		
+		
 		
 	}
 
